985437a5273cdcac4f8515c86a5f849c36c565f1
tomlingb <tomlingb@rose-hulman.edu>
2017-12-07 11:27:01 -0500

    TODO 8

--- src/m1.py
+++ src/m1.py
@@ -295,7 +295,7 @@ def broken_5(circle, window):
 
 
 # ----------------------------------------------------------------------
-# TODO: 8. Follow the INSTRUCTIONS AT THE TOP OF THIS MODULE
+# DONE: 8. Follow the INSTRUCTIONS AT THE TOP OF THIS MODULE
 #          to correct the mistake(s) in the following function.
 # ----------------------------------------------------------------------
 def broken_6(n):
@@ -306,8 +306,8 @@ def broken_6(n):
     Side effects:   None.
     """
     total = 0
-    for k in range(n + 1):
-        total = total + 1 / k
+    for k in range(n):
+        total = total + 1 / (k + 1)
 
     return total
 
